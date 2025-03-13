06038c1561aa1233d840c74efaf351046bcd9054
daxp472 <daxpatel.cg@gmail.com>
2025-03-14 00:00:34 +0530

    Achivement route added with the current positing feature for like #1 in this game and so on

---
+++ Updated Backend/XP and Stats/src/routes/achievementRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { body } = require('express-validator');
+const achievementController = require('../controllers/achievementController');
+
+const router = express.Router();
+
+router.post('/add', [
+  body('email').isEmail(),
+  body('game').notEmpty(),
+  body('position').isInt({ min: 1 })
+], achievementController.addAchievement);
+
+router.get('/:email', achievementController.getAchievements);
+
+module.exports = router;
