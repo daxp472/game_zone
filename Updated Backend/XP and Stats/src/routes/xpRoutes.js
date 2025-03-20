0183f22734edba96ac07bca4a9d9b2cee7bcdd50
daxp472 <daxpatel.cg@gmail.com>
2025-03-20 16:07:33 +0530

    XP /all and /:email option added

--- Updated Backend/XP and Stats/src/routes/xpRoutes.js
+++ Updated Backend/XP and Stats/src/routes/xpRoutes.js
@@ -5,10 +5,11 @@ const xpController = require('../controllers/xpController');
 const router = express.Router();
 
 router.post('/add', [
-  body('email').isEmail(),
-  body('xpAmount').isInt({ min: 1 })
+    body('email').isEmail(),
+    body('xpAmount').isInt({ min: 1 })
 ], xpController.addXP);
 
 router.get('/:email', xpController.getXP);
+router.get('/all', xpController.getAllXP);
 
 module.exports = router;
