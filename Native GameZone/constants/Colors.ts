37b5440e8db5eafeaa07667b14ef7a81096597cc
daxp472 <daxpatel.cg@gmail.com>
2025-05-05 11:38:55 +0530

    COlor.ts file added with multi theme option

---
+++ Native GameZone/constants/Colors.ts
@@ -0,0 +1,22 @@
+export const Colors = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: '#ff4500', // Neon orange
+    border: '#ccc',
+    placeholder: '#999',
+    spinWheel: '#ff6347',
+    gradientStart: '#ff4500', // Gradient for backgrounds
+    gradientEnd: '#8a2be2', // Neon purple
+  },
+  dark: {
+    text: '#fff',
+    background: '#121212',
+    tint: '#ff6347',
+    border: '#444',
+    placeholder: '#777',
+    spinWheel: '#ff4500',
+    gradientStart: '#1a1a2e', // Dark blue
+    gradientEnd: '#16213e', // Darker blue
+  },
+};
