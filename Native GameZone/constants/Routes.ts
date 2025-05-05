fbf3d848fdb425dc8f7d0013537aa6b786520376
daxp472 <daxpatel.cg@gmail.com>
2025-05-05 11:38:23 +0530

    constants folder seprated and Routes.ts filed added hear

---
+++ Native GameZone/constants/Routes.ts
@@ -0,0 +1,13 @@
+export const Routes = {
+    INTRO: 'intro',
+    ACCESS: 'access',
+    LOGIN: 'login',
+    SIGNUP: 'signup',
+    TABS: '(tabs)',
+    HOME: 'index',
+    MULTIPLAYER: 'multiplayer',
+    SPIN_WHEEL: 'spin-wheel',
+    TOURNAMENT: 'tournament',
+    PROFILE: 'profile',
+    NOT_FOUND: '+not-found',
+  } as const;
